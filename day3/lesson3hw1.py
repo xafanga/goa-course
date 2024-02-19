8a11b628f28386d3c528986d699c37c3a2bcff6e
xafanga <siradze.andria2013@gmail.com>
2024-02-19 21:42:23 +0400

    commit

---
+++ day3/lesson3hw1.py
@@ -0,0 +1,33 @@
+nameFather= 'irakli'
+surnameFather = 'siradze'
+
+
+nameMother = 'tamta'
+surnameMother = 'goloshvili'
+
+
+nameBrother1 = 'gio'
+surnameBrother1 = 'akhalmosulishvili'
+
+
+nameBrother2 = 'luka'
+surnameBrother2 = 'siradze'
+
+
+nameGrandma1 = 'nato'
+surnameGrandma1 = 'apkhadze'
+
+
+nameGrandma2 = 'tsiala'
+surnameGrandma2 = 'shavishvili'
+
+
+nameGrandpa1 = 'gia'
+surnameGrandpa1 = 'goloshvili'
+
+
+nameGrandpa2 = 'tengiz'
+surnameGrandpa2 = 'siradze'
+
+
+print(nameFather + ' ' + surnameFather + ' ' + nameMother + ' ' + surnameMother + " " + nameBrother1 + ' ' + surnameBrother1 + ' ' + nameBrother2 + " " + surnameBrother2 + ' ' + nameGrandma1 + ' ' + surnameGrandma1 + ' ' + nameGrandma2 + ' ' + surnameGrandma2 + ' ' + nameGrandpa1 + ' ' + surnameGrandpa1 + ' ' + nameGrandpa2 + ' ' + surnameGrandpa2 )
