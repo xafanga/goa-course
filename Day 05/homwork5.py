311527aebe3036bdee6b47ef2e179fd1649b7131
xafanga <siradze.andria2013@gmail.com>
2024-03-13 14:53:53 +0400

    first commit

--- Day 05/homwork5.py
+++ Day 05/homwork5.py
@@ -1,14 +1,14 @@
-today is great weather
-the tbilisi is city
-rearrange your items
-jimmy dont use parentheses
-yesterday we bought various things
-lilys test is incorrect
-I loss my compiuter
-i select GOA to study programming
-I handle with the first stage
-I tell my mother that I want to become programmer
-I survey my brother
-I ensure with school transport to went in te school every day
-I drew a explicitly line
-my building and your building are made of similar materials
+"today is great weather"
+"the tbilisi is city"
+"rearrange your items"
+"jimmy dont use parentheses"
+"yesterday we bought various things"
+"lilys test is incorrect"
+"I loss my compiuter"
+"i select GOA to study programming"
+"I handle with the first stage"
+"I tell my mother that I want to become programmer"
+"I survey my brother"
+"I ensure with school transport to went in te school every day"
+"I drew a explicitly line"
+"my building and your building are made of similar materials"
---
+++ Day 06/homework6.py
@@ -0,0 +1,51 @@
+name = input ("Enter your name : ")
+surname = input ("Enter your surname : ")
+age = input ("Enter your age : ")
+hobby = input ("Enter your hobby : ")
+Dwelling_place = input ("Enter your Dwelling_place : " )
+print("hi my name is Andria my dwelling_place is tbilisi my surname is siradze my hobby is playing football and my 11 years old  ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+number = input("Enter the number : ")
+number2 = input("Enter the number : ")
+print(10+28)
+print(28-10)
+print(10*28)
+print(28//10)
+
+
+
+
+
+
+
+
+
+
+
+
+
+mothersage = input("enter your mothers age : ")
+fathersage = input("enter your fathers age : ")
+brothersage = input("enter your brathes age : ")
+grandmasage = input("enter your grandmas age : ")
+grandpasage = input("enter your grandpas age : ")
+print('my mother will be 65 years old after 25 years')
+print('my father will be 68 years old after 25 years')
+print('my brother will be 44 years old after 25 years')
+print('my grandma will be 88 years old after 25 years')
+print('my grandpa will be 108 years old after 25 years')
