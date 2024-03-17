db096558a198b33cb2756dc907a1bf687b065142
xafanga <siradze.andria2013@gmail.com>
2024-03-17 18:55:44 +0400

    first commit

--- Day 06/homework6.py
+++ Day 06/homework6.py
@@ -1,9 +1,10 @@
+#მომხმარებელს შემოატანინეთ თავისი სახელი, გვარი, ასაკი, საცხოვრებელი ადგილი, პროფესია, ჰობი და კიდევ დამატებითი ინფორმაცია, შემდეგ ამ შემოტანილი ინფორმაციით ააწყვეთ ერთი დიდი წინადადება.
 name = input ("Enter your name : ")
 surname = input ("Enter your surname : ")
 age = input ("Enter your age : ")
 hobby = input ("Enter your hobby : ")
 Dwelling_place = input ("Enter your Dwelling_place : " )
-print("hi my name is Andria my dwelling_place is tbilisi my surname is siradze my hobby is playing football and my 11 years old  ")
+print(name + " " + surname + " " + "is" + " " + age + " " + "years old" + " " + name + " " + surname + " " + "lives in" + " " + Dwelling_place + " " + name + " " + surname + " " + "hobby is" + hobby )
 
 
 
@@ -19,15 +20,13 @@ print("hi my name is Andria my dwelling_place is tbilisi my surname is siradze m
 
 
 
-
-number = input("Enter the number : ")
-number2 = input("Enter the number : ")
-print(10+28)
-print(28-10)
-print(10*28)
-print(28//10)
-
-
+#მომხმარებელს შემოატანინეთ ორი სხვადასხვა რიცხვი და ამ შემოტანილ რიცხვებზე მოახდინეთ მათემათიკური ოპერაციები, როგორიცაა +,-,*,/
+number1 = int(input("Enter the first number : "))
+number2 = int(input("Enter the second number : "))
+print(number1+number2)
+print(number2-number1)
+print(number1*number2)
+print(number2//number1)
 
 
 
@@ -39,11 +38,13 @@ print(28//10)
 
 
 
-mothersage = input("enter your mothers age : ")
-fathersage = input("enter your fathers age : ")
-brothersage = input("enter your brathes age : ")
-grandmasage = input("enter your grandmas age : ")
-grandpasage = input("enter your grandpas age : ")
+
+#შემოიტანეთ თქვენი ოჯახის წევრების ასაკი და დაუპრინტეთ თუ რამდენი წლისები იქნებიან 25 წლის შემდეგ.
+mothersage = int(input("enter your mothers age : "))
+fathersage = int(input("enter your fathers age : "))
+brothersage = int(input("enter your brathes age : "))
+grandmasage = int(input("enter your grandmas age : "))
+grandpasage = int(input("enter your grandpas age : "))
 print('my mother will be 65 years old after 25 years')
 print('my father will be 68 years old after 25 years')
 print('my brother will be 44 years old after 25 years')
---
+++ homework7.py
@@ -0,0 +1,51 @@
+# მომხმარებელს შემოიტანეთ 3 სხვადასხვა მონაცემტა ტიპი  და გაიგეთ თუ რომელ მონაცემტა ტიპს მიეკუთვნება შემოტანილი ინფორმაცია
+age = input("Enter your age : ")
+print(type(age))
+balance = input("Enter balance")
+print(type(balance))
+cost = input("Enter cost")
+print(type(cost))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# მომხმარებელს შემოატანინეთ 5  სხვადასხვა რიცხვი და ამ შემოტანილ რიცხვებზე მოახდინეთ მათემათიკური ოპერაციები, როგორიცაა +,-,*,/ 
+num1 = int(input("Enter num1 : "))
+num2 = int(input("Enter num2 : "))
+num3 = int(input("Enter num3 : "))
+num4 = int(input("Enter num4 : "))
+num5 = int(input ("Enter num5 : "))
+print(num1 + num2)
+print(num1 - num3)
+print(num1 * num4)
+print(num1//num5)
+print(num2 + num1)
+print(num2 - num3)
+print(num2 * num4)
+print(num2 // num5)
+print(num3 + num1)
+print(num3 - num2)
+print(num3 * num4)
+print(num3 // num5)
+print(num4 + num1)
+print(num4 - num2)
+print(num4 * num3)
+print(num4 // num5)
+print(num5 + num1)
+print(num5 - num2)
+print(num5 * num3)
+print(num5 // num4)
